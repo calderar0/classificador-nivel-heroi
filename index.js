866adfc81ecfb528c0c1fe3fda643b55552b6daa
calderar0 <felipecalderaro28@gmail.com>
2024-05-06 16:35:26 -0300

    Projeto Finalizado

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
-let heroi = [
-    ["HeroName", 10001]
-]
+function verificarNivel(){
 
-let nivel = " "
-if (heroi[0][1] <= 1000) {
-    nivel = "Ferro"
-}else if (heroi[0][1] <= 2000) {
-    nivel = "Bronze"
-}else if (heroi[0][1] <= 5000) {
-    nivel = "Prata"
-}else if (heroi[0][1] <= 7000) {
-    nivel = "Ouro"
-}else if (heroi[0][1] <= 8000) {
-    nivel = "Platina"
-}else if (heroi[0][1] <= 9000) {
-    nivel = "Ascendente"
-}else if (heroi[0][1] <= 10000) {
-    nivel = "Imortal"
-}else if (heroi[0][1] > 10000) {
-    nivel = "Radiante"
-}
+    let heroi = [
+        [document.getElementById('heroi').value, document.getElementById('pontos').value]
+    ]
 
+    heroi[0][0] = heroi[0][0] === '' ? 'Desconhecido' : heroi;
 
-console.log("O Herói de nome "  + heroi[0][0] + " está no nível de " + nivel)
+    let nivel = " "
+    if (heroi[0][1] <= 1000) {
+        nivel = "Ferro"
+    }else if (heroi[0][1] <= 2000) {
+        nivel = "Bronze"
+    }else if (heroi[0][1] <= 5000) {
+        nivel = "Prata"
+    }else if (heroi[0][1] <= 7000) {
+        nivel = "Ouro"
+    }else if (heroi[0][1] <= 8000) {
+        nivel = "Platina"
+    }else if (heroi[0][1] <= 9000) {
+        nivel = "Ascendente"
+    }else if (heroi[0][1] <= 10000) {
+        nivel = "Imortal"
+    }else if (heroi[0][1] > 10000) {
+        nivel = "Radiante"
+    }
+    console.log("O Herói de nome "  + heroi[0][0] + " está no nível de " + nivel)
+    
+    let elemento = document.getElementById('resultado')
+    elemento.classList.add('resultado')
+    
+    document.getElementById('result').textContent = `O Herói de nome ${heroi[0][0]} está no nível de ${nivel}`
+}   
